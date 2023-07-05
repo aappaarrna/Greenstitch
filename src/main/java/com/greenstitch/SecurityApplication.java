dbe42470d45b02fd87f2eece4b817c46fe73d116
Aparna Tiwar <aparna.tiwar@Pratian.com>
2023-07-05 16:52:54 +0530

    Code Pushed

---
+++ src/main/java/com/greenstitch/SecurityApplication.java
@@ -0,0 +1,48 @@
+package com.greenstitch;
+
+import com.greenstitch.security.auth.AuthenticationService;
+import com.greenstitch.security.auth.RegisterRequest;
+import com.greenstitch.security.user.Role;
+
+import static com.greenstitch.security.user.Role.ADMIN;
+import static com.greenstitch.security.user.Role.MANAGER;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(
+			AuthenticationService service
+	) {
+		return args -> {
+			var admin = RegisterRequest.builder()
+					.firstname("Admin")
+					
+					.lastname("Admin")
+					.email("admin@mail.com")
+					.password("password")
+					.role(ADMIN)
+					.build();
+			System.out.println("Admin token: " + service.register(admin).getAccessToken());
+
+			var manager = RegisterRequest.builder()
+					.firstname("Admin")
+					.lastname("Admin")
+					.email("manager@mail.com")
+					.password("password")
+					.role(MANAGER)
+					.build();
+			System.out.println("Manager token: " + service.register(manager).getAccessToken());
+
+		};
+	}
+}
